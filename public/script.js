c3d4da4592dcedc5ec261ec6b59e0e8cb5a48348
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-02-14 22:44:31 +0600

    para done

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ function calculateTriangleArea(){
         let area = 'invalid input';
         
         document.getElementById('triSpan').innerText = area;
-        console.log(area);
+        
         return
     }
 
@@ -24,14 +24,78 @@ function calculateTriangleArea(){
    document.getElementById('asideUl').appendChild(list)
     
     
-    // console.log(area)
+   
 
 
-
+    document.getElementById('triBase').value = "";
+    document.getElementById('triHight').value = "";
 
 
 
 }
+
+let recBtn = document.getElementById('recBtn');
+
+
+recBtn.addEventListener('click',calculateRectangleArea);
+function calculateRectangleArea(){
+    let recWidthValue = parseFloat(document.getElementById('recWidth').value);
+
+    let recHightValue = parseFloat(document.getElementById('recHight').value);
+
+    if(isNaN(recWidthValue) || isNaN(recHightValue) ){
+        let recArea = 'invalid input'
+        document.getElementById('recSpan').innerHTML = recArea ;
+        return
+    }
+
+    let recArea = recHightValue*recWidthValue + " cm2";
+    document.getElementById('recSpan').innerHTML = recArea ;
+
+
+    let list =  document.createElement('li');
+    list.innerText=` RectangleArea : w= ${recWidthValue} x h=${recHightValue} = ${recArea}`;
+    document.getElementById('asideUl').appendChild(list)
+    document.getElementById('recWidth').value = "";
+    document.getElementById('recHight').value = "";
+    
+        return
+    
+}
+
+
+
+
+let paraBtn = document.getElementById('paraBtn');
+
+
+paraBtn.addEventListener('click',calculateParallelogramArea);
+function calculateParallelogramArea(){
+    let paraBaseValue = parseFloat(document.getElementById('paraBase').value);
+
+    let paraHightValue = parseFloat(document.getElementById('paraHight').value);
+
+    if(isNaN(paraBaseValue) || isNaN(paraHightValue) ){
+        let paraArea = 'invalid input'
+        document.getElementById('paraSpan').innerHTML = paraArea ;
+        return
+    }
+
+    let paraArea = paraBaseValue*paraHightValue + " cm2";
+    document.getElementById('paraSpan').innerHTML = paraArea ;
+
+
+    let list =  document.createElement('li');
+    list.innerText=`parallelogramArea : w= ${paraBaseValue} x h=${paraHightValue} = ${paraArea}`;
+    document.getElementById('asideUl').appendChild(list)
+    document.getElementById('paraBase').value = "";
+    document.getElementById('paraHight').value = "";
+    
+        return
+    
+}
+
+
 
 
 
